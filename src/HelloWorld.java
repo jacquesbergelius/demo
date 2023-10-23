0a9bbbfdb60963b2c3098b774aef102c62c077e1
Jarkko Vuori <jarkkov@metropolia.fi>
2023-10-23 12:00:18 +0300

    superHumanMain method added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,10 @@
 // This is a comment
 public class HelloWorld {
-/*    public static void superHumanMain(String[] args) {
+    public static void superHumanMain(String[] args) {
         System.out.println("Hello Beautiful World!");
         System.out.println("This is a second line");
     }
-*/
+
     public static void main(String[] args) {
         System.out.println("I am here!");
     }
