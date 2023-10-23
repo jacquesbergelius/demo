86d4ae04bc5e2857287cc972f03972f5ff55596e
Jarkko Vuori <jarkkov@metropolia.fi>
2023-10-23 11:52:30 +0300

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+// This is a comment
+public class HelloWorld {
+/*    public static void superHumanMain(String[] args) {
+        System.out.println("Hello Beautiful World!");
+        System.out.println("This is a second line");
+    }
+*/
+    public static void main(String[] args) {
+        System.out.println("I am here!");
+    }
+}
